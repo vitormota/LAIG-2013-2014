9b8f52d0ef7168f6c94516a2f61a734112ab7b09
maria- <mj@MacBook-Air-de-Maria.local>
2013-09-28 02:05:06 +0100

    Commit to revert to the previous changes (Lightning class)

--- include/Lightning.h
+++ include/Lightning.h
@@ -11,43 +11,72 @@
 
 	using namespace std;
 
+<<<<<<< HEAD
 	class Lightning/*:public CGFlight*/{
         
     public:
         Lightning();
         ~Lightning();
+=======
+	typedef struct{
+		bool doublesided;
+		bool local;
+		bool enabled;
+		float ambient[4];
+	} global_lightning_vars;
 
+	class Light:public CGFlight{
+	public:
+		Light(string id,bool enabled,unsigned int light_id,
+			float *pos,float location[4],float ambient[4],
+			float difuse[4],float specular[4],float *dir=NULL);
+		virtual ~Light();
+>>>>>>> b025d1450191b30cdd6a03f0487632930308d71e
+
+		string getId()const {return id;}
+		bool isEnabled()const {return enabled;}
+
+		//pure virtual makes class abstract
+		virtual float getVars()const =0;
+	protected:
+		string id;
+		bool enabled;
+		float
+			*location,
+			*ambient,
+			*difuse,
+			*specular;
 	};
 
 	class Omni:public Lightning{
 	public:
-		Omni(string id,bool enabled,float *location,float *ambient,float *difuse,float *specular);
+		Omni(string id,bool enabled,unsigned int light_id,
+			float *pos,float location[4],float ambient[4],
+			float difuse[4],float specular[4],float *dir=NULL);
 		~Omni();
+
+		
 	private:
-		string id;
-		bool enabled;
-		float
-			location[4],
-			ambient[4],
-			difuse[4],
-			specular[4];
+		
 	};
 
 	class Spot:public Lightning{
 	public:
+<<<<<<< HEAD
 		Spot(string id, bool enabled, float *location, float *ambient,float *difuse, float *specular, float angle, float exponent,float *direction);
+=======
+		Spot(string id,bool enabled,unsigned int light_id,
+			float *pos,float location[4],float ambient[4],
+			float difuse[4],float specular[4],float angle,
+			float exponent, float direction[3],float *dir=NULL);
+>>>>>>> b025d1450191b30cdd6a03f0487632930308d71e
 		~Spot();
+
 	private:
-		string id;
-		bool enabled;
 		float
-			location[4],
-			ambient[4],
-			difuse[4],
-			specular[4],
 			angle,
 			exponent,
-			direction[3];
+			*direction;
 	};
 
 #endif
--- src/Lightning.cpp
+++ src/Lightning.cpp
@@ -3,10 +3,54 @@
 
 	using namespace std;
 
+<<<<<<< HEAD
 Lightning::Lightning()
 {
     
 }
+=======
+	Light::Light(string id,bool enabled,unsigned int light_id,
+			float *pos,float location[4],float ambient[4],
+			float difuse[4],float specular[4],float *dir):CGFlight(light_id,pos,dir){
+			this->enabled=enabled;
+			this->id = id;
+			this->location=location;
+			this->specular =specular;
+			this->ambient=ambient;
+			this->difuse=difuse;
+			
+	}
+
+	Light::~Light(){
+		delete [] &location;
+		delete [] &ambient;
+		delete [] &difuse;
+		delete [] &specular;
+	}
+
+	Omni::Omni(string id,bool enabled,unsigned int light_id,
+			float *pos,float location[4],float ambient[4],
+			float difuse[4],float specular[4],float *dir)
+		:Light(id,enabled,light_id,pos,location,ambient,difuse,specular,dir){}
+
+	Omni::~Omni(){
+	}
+
+	Spot::Spot(string id,bool enabled,unsigned int light_id,
+			float *pos,float location[4],float ambient[4],
+			float difuse[4],float specular[4],float angle,
+			float exponent, float direction[4],float *dir)
+		:Light(id,enabled,light_id,pos,location,ambient,difuse,specular,dir){
+			this->angle=angle;
+			this->exponent=exponent;
+			this->direction=direction;
+	}
+
+	Spot::~Spot(){
+		delete [] &direction;
+	}
+
+>>>>>>> b025d1450191b30cdd6a03f0487632930308d71e
 
 Lightning::~Lightning()
 {
